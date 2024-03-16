b4fc5c1cb0c0041b12fd62e9f2902577c66d152b
XiaNight <summernight2001519@gmail.com>
2024-03-16 14:57:22 +0800

    feat(): one hot, fit, evaluate

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import autokeras as ak
 train_data = np.load('train_data.npy')
 test_data = np.load('test_data.npy')
 
+layer = keras.layers.CategoryEncoding(num_tokens=2, output_mode="one_hot")
+test_data = layer.adapt(test_data)
+
 # split data
 X_train, X_test, y_train, y_test = train_test_split(
     train_data, test_data, test_size=0.2, random_state=42)
@@ -31,3 +34,11 @@ plt.hist(y_test, bins=bin, edgecolor='black')
 plt.title('Test Data')
 
 plt.show()
+
+# if outcome is not good, increase max_trials
+clf = ak.ImageClassifier(project_name='my_image_model', max_trials=3)
+clf.fit(X_train, y_train)
+
+clf.evaluate(X_test, y_test)
+
+predicted_y = clf.predict(X_test[:10])
